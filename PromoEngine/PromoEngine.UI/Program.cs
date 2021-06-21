79f4e22be5b3a08ea7cb45ff7b64db4f1e98f4f3
Alex Pradais <fyraxis@yahoo.com>
2021-06-21 19:51:32 +0300

    Minimal setup of components

--- PromoEngine/PromoEngine.UI/Program.cs
+++ PromoEngine/PromoEngine.UI/Program.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using PromoEngine.EngineLib;
+using System;
 
 namespace PromoEngine.UI
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            ShoppingCart cart;
+            PromoEvalEngine engine;
+            LoadShoppingCartData(out cart);
+            InitPromoEngine(out engine);
+            ProcessCartContents(engine, cart);
+        }
+
+        private static void LoadShoppingCartData(out ShoppingCart cart)
+        {
+            cart = new ShoppingCart();
+        }
+
+        private static void InitPromoEngine(out PromoEvalEngine engine)
+        {
+            engine = new PromoEvalEngine();
+        }
+
+        private static void ProcessCartContents(PromoEvalEngine engine, ShoppingCart cart)
         {
             
         }
