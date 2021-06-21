c28ae196d4ee79fb5fc74416d93227f3c6f2d073
Alex Pradais <fyraxis@yahoo.com>
2021-06-21 20:38:16 +0300

    add tests project

---
+++ PromoEngine/PromoEngine.Tests/PromoTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PromoEngine.Tests
+{
+    public class PromoTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
